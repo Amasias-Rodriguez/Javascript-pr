209318ae560629b15e67a3291f8557c46dcf7c29
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2024-01-23 01:38:41 -0600

    Update main.js

--- jr-pr/main.js
+++ jr-pr/main.js
@@ -20,3 +20,15 @@ resetBtn.onclick = function(){
     countlabel.textContent = count;
 }
 
+//Random num generator
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum
+}
