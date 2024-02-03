71520b3790cbb0e7bf4e6aa353a3528838fa3dcf
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2024-02-03 00:03:58 -0600

    Update main.js

--- jr-pr/main.js
+++ jr-pr/main.js
@@ -83,3 +83,22 @@ function convert(){
         result.textContent = "Select a unit";
     }
 }
+
+//Roll dices
+
+function rollDice(){
+    const numbOfDice = document.getElementById("numbOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numbOfDice; i++){
+        const value = Math.floor(Math.random()* 6) + 1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(',')}`;
+    diceImages.innerHTML = images.join('');
+}
